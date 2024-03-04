f89f25f66d613173487e6cf1704345e805db18e4
Gpocas <gpocas01@gmail.com>
2024-03-04 16:56:36 -0300

    initial commit

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"craftzdog/solarized-osaka.nvim",
+		lazy = true,
+		priority = 1000,
+		opts = function()
+			return {
+				flavour = "mocha",
+				transparent = true,
+			}
+		end,
+	},
+}
